1f0ecb184b09cb8fa4286168126bb6f90f0725e0
Systemad <dannepuke@gmail.com>
2020-11-14 21:16:03 +0100

    Added missing text functions
    It is now possible to use follwing from Edit menu.
    - Copy
    - Paste
    - Cut
    - Undo
    - Redo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -85,3 +85,28 @@ void MainWindow::on_actionSave_triggered()
         QFile.close();
     }
 }
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
